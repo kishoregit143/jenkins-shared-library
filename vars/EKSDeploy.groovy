f162db10b956fb5fd6b952b3823d5589242b7b5b
kishore <pagadalamokisthu@gmail.com>
2026-04-21 15:40:25 +0530

    new-feature for the dev

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -34,7 +34,7 @@ pipeline {
                             set -e
                             aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
                             kubectl get nodes
-                            sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
+                            sed -i "s/IMAGE_VERSION/${appVersion}/g" values-${deploy_to}.yaml
                             helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
                         """
                     }
